8dc2e2f7b029d51e84bdede27cd87a679e840d2c
GoodGuyGregory <greg.witt625@gmail.com>
2024-03-01 09:47:46 -0500

    reads in csv data

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import pandas as pd
+import numpy as np
+
+def readInData():
+    clusterData = pd.read_csv("545_cluster_dataset programming 3.txt", header=None,)
 
 
 def main():
-    print('works')
+    readInData()
 
 main()
