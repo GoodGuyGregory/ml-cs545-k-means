7e7d00c4f9cb944a5943b49eb03c2bda1e5b8b60
GoodGuyGregory <greg.witt625@gmail.com>
2024-03-01 12:23:27 -0500

    adds two feature split for data

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
 
-def readInData():
-    clusterData = pd.read_csv("545_cluster_dataset programming 3.txt", header=None,)
-
+def readData():
+    clusterData = pd.read_csv("545_cluster_dataset programming 3.txt", sep='\t', header=None)
+    clusterData[['Feature 1', 'Feature 2']] = clusterData[0].str.split(expand=True, n=1)
+    clusterData.drop(0, axis=1, inplace=True)
+    return clusterData
 
 def main():
-    readInData()
+    clusterPoints = readData()
+    print(clusterPoints)
+
 
 main()
