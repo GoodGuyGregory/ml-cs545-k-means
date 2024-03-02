b399b8cb39538bbbcdf8b58ca69b55868b575110
GoodGuyGregory <greg.witt625@gmail.com>
2024-03-01 22:00:20 -0500

    adds classifier for Kmeans

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import pandas as pd
 import numpy as np
 
+
+class KMeansCluster:
+    def __int__(self, k, iterations, dataSet):
+        # number of clusters
+        self.K = k
+        self.iterations = iterations
+        self.dataSet = dataSet
+
+        self.centroid = []
+
+
+
+
+
+
 def readData():
     clusterData = pd.read_csv("545_cluster_dataset programming 3.txt", sep='\t', header=None)
     clusterData[['Feature 1', 'Feature 2']] = clusterData[0].str.split(expand=True, n=1)
