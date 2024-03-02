0fa84ffe1e2e891c3baaa76d6737d869fd2c6724
GoodGuyGregory <greg.witt625@gmail.com>
2024-03-02 01:06:58 -0500

    adds random cetroid method and begins clustering distance measurement

--- main.py
+++ main.py
@@ -2,16 +2,34 @@ import pandas as pd
 import numpy as np
 
 
-class KMeansCluster:
+class KMeansClassifier:
     def __int__(self, k, iterations, dataSet):
         # number of clusters
         self.K = k
         self.iterations = iterations
         self.dataSet = dataSet
 
-        self.centroid = []
+        # stores mean values of the centroids
+        self.centroids = []
+        # dataframe that holds the centroid minArgResults from kClassifyCall
+        self.centroidPoints =
+
+    def randomizeKCentroids(self):
+        # randomly pick K centroids from the data values
+        for k in self.K:
+            # choose a random sample point
+            randomCentroid = self.dataSet.sample(frac=1)
+            self.centroids.append(randomCentroid)
 
 
+    def kMeansClassify(self):
+        # determine distance from the closest cluster
+        pass;
+
+
+
+    def fuzzyCmeansClassify(self):
+        return None
 
 
 
