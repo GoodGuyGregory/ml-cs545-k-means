1aeff854287155a1deb6eca45bb42ade32594bc3
GoodGuyGregory <greg.witt625@gmail.com>
2024-03-05 09:23:26 -0500

    adds centroid distance metric

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class KMeansClassifier:
         # number of clusters
         self.K = k
         self.iterations = iterations
-        self.dataSet = np.asarray(dataSet, dtype=float)
+        self.dataSet = dataSet
 
         # stores mean values of the centroids
         self.centroids = []
@@ -16,14 +16,21 @@ class KMeansClassifier:
 
     def randomizeKCentroids(self):
         # randomly pick K centroids from the data values
-        self.centroids = np.random.uniform(np.amin(self.dataSet), np.amax(self.dataSet), size=self.K)
+        self.centroids = np.random.uniform(np.amin(self.dataSet), np.amax(self.dataSet), size=(self.K, self.dataSet.shape[1]))
         print(self.centroids)
-
 
 
     def kMeansClassify(self):
         # determine distance from the closest cluster
-        pass
+        clusterClasses = []
+
+        # calculate the distance metric per point
+        for point in self.dataSet:
+            closestValue = np.argmin(np.sqrt(np.sum((point - self.centroids)**2, axis=1)))
+            closetCluster = closestValue
+            clusterClasses.append(closetCluster)
+
+
 
 
 
@@ -34,16 +41,14 @@ class KMeansClassifier:
 
 
 def readData():
-    clusterData = pd.read_csv("545_cluster_dataset programming 3.txt", sep='\t', header=None)
-    clusterData[['Feature 1', 'Feature 2']] = clusterData[0].str.split(expand=True, n=1)
-    clusterData.drop(0, axis=1, inplace=True)
+    clusterData = np.loadtxt("545_cluster_dataset programming 3.txt")
     return clusterData
 
 def main():
     clusterPoints = readData()
-    print(clusterPoints)
     kmeans = KMeansClassifier(5,10, clusterPoints)
     kmeans.randomizeKCentroids()
+    kmeans.kMeansClassify()
 
 
 main()
